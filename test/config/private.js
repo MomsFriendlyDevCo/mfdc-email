1494b7bdd9ed71be6bf4799167f73f266f6fc3da
Matt Carter <m@ttcarter.com>
2016-10-25 12:34:35 +1100

    Added private key file layout

---
+++ test/config/private.js
@@ -0,0 +1,8 @@
+// This file is private and SHOULD NOT be added into git
+// Enter stuff here that you DO NOT want to be synced. For example unique database auth details, private keys etc
+module.exports = {
+	mailgun: {
+		apiKey: 'ENTER KEY HERE',
+		domain: 'mfdc.biz',
+	},
+};
