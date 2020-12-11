63785d72c956f9a3176928649a818222e9776b34
Matt Carter <m@ttcarter.com>
2020-12-11 13:32:50 +1000

    FEAT: Added support for SMTP + Outlook365 methods

---
+++ test/outlook365.js
@@ -0,0 +1,37 @@
+var email = require('..');
+var expect = require('chai').expect;
+
+global.config = require('./config');
+
+describe('Outlook365 > Send', function() {
+	this.timeout(30 * 1000);
+
+	before(function() {
+		config.email.enabled = true;
+		config.email.method = 'outlook365';
+	});
+
+	it('should send a plain email', function(done) {
+		email()
+			.send({
+				subject: 'Plain email test via mfdc-email',
+				text: 'Hello World',
+			}, function(err, res) {
+				expect(err).to.be.not.ok;
+				// FIXME: Not sure what res should be
+				done();
+			});
+	});
+
+	it('should send a HTML email', function(done) {
+		email()
+			.send({
+				subject: 'HTML email test via mfdc-email',
+				html: '<p>Hello <b>World</b></p>',
+			}, function(err, res) {
+				expect(err).to.be.not.ok;
+				// FIXME: Not sure what res should be
+				done();
+			});
+	});
+});
